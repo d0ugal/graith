d1f96823800ade72cd2a086bea9dd97991f194c2
Dougal Matthews <dougal@grafana.com>
2026-06-06 12:55:01 +0100

    feat: add config loading, path resolution, and template expansion

--- go.mod
+++ go.mod
@@ -5,6 +5,9 @@ go 1.26.3
 require github.com/spf13/cobra v1.10.2
 
 require (
+	github.com/adrg/xdg v0.5.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.3.1 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
+	golang.org/x/sys v0.26.0 // indirect
 )
---
+++ internal/config/paths.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/adrg/xdg"
+)
+
+const appName = "graith"
+
+type Paths struct {
+	ConfigFile string
+	DataDir    string
+	RuntimeDir string
+	SocketPath string
+	PIDFile    string
+	StateFile  string
+	LogDir     string
+	DaemonLog  string
+}
+
+func ResolvePaths() Paths {
+	configFile := filepath.Join(xdg.ConfigHome, appName, "config.toml")
+	dataDir := filepath.Join(xdg.DataHome, appName)
+	runtimeDir := runtimeDirForGraith()
+
+	return Paths{
+		ConfigFile: configFile,
+		DataDir:    dataDir,
+		RuntimeDir: runtimeDir,
+		SocketPath: filepath.Join(runtimeDir, "graith.sock"),
+		PIDFile:    filepath.Join(runtimeDir, "graith.pid"),
+		StateFile:  filepath.Join(dataDir, "state.json"),
+		LogDir:     filepath.Join(dataDir, "logs"),
+		DaemonLog:  filepath.Join(dataDir, "daemon.log"),
+	}
+}
+
+func runtimeDirForGraith() string {
+	if d := xdg.RuntimeDir; d != "" {
+		return filepath.Join(d, appName)
+	}
+	if d := os.Getenv("TMPDIR"); d != "" {
+		return filepath.Join(d, fmt.Sprintf("graith-%d", os.Getuid()))
+	}
+	return filepath.Join("/tmp", fmt.Sprintf("graith-%d", os.Getuid()))
+}
+
+func (p Paths) EnsureDirs() error {
+	dirs := []string{filepath.Dir(p.ConfigFile), p.DataDir, p.RuntimeDir, p.LogDir}
+	for _, dir := range dirs {
+		if err := os.MkdirAll(dir, 0o750); err != nil {
+			return fmt.Errorf("create directory %s: %w", dir, err)
+		}
+	}
+	return nil
+}
