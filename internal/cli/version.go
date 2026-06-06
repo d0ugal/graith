2a842886acd286260e267aca1225f061792cda4c
Dougal Matthews <dougal@grafana.com>
2026-06-06 19:48:41 +0100

    feat: add gr version command
    Prints version and commit SHA. Supports --json flag.

---
+++ internal/cli/version.go
@@ -0,0 +1,25 @@
+package cli
+
+import (
+	"github.com/dougalmatthews/graith/internal/version"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print version information",
+	Run: func(cmd *cobra.Command, args []string) {
+		if jsonOutput {
+			out.JSON(struct {
+				Version string `json:"version"`
+				Commit  string `json:"commit"`
+			}{version.Version, version.CommitSHA})
+			return
+		}
+		out.Print("graith %s (%s)\n", version.Version, version.CommitSHA)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
