4fcad20f1851f6e29f55d9f2764eaea8033085c4
Dougal Matthews <dougal@grafana.com>
2026-08-23 09:14:43 +0100

    fix: isolate Linux upgrade sealing

--- internal/daemon/upgrade_target_pin.go
+++ internal/daemon/upgrade_target_pin.go
@@ -5,7 +5,6 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"errors"
-	"fmt"
 	"io"
 	"os"
 	"os/exec"
@@ -68,37 +67,6 @@ func pinUpgradeTarget(path string) (_ *upgradeTargetPin, returnErr error) {
 	keep = true
 
 	return pin, nil
-}
-
-func (p *upgradeTargetPin) retainLinuxCopy() error {
-	retained, err := executablepin.SealedCopy(p.file, p.info.Size(), "graith-upgrade-target")
-	if err != nil {
-		return err
-	}
-
-	retainedInfo, err := retained.Stat()
-	if err != nil || !retainedInfo.Mode().IsRegular() || retainedInfo.Mode().Perm()&0o111 == 0 || retainedInfo.Size() != p.info.Size() {
-		_ = retained.Close()
-		return errors.New("retained upgrade target metadata is unsafe")
-	}
-
-	retainedDigest, err := digestUpgradeTargetFile(retained, retainedInfo.Size())
-	if err != nil || retainedDigest != p.digest {
-		_ = retained.Close()
-		return errors.New("retained upgrade target content differs")
-	}
-
-	if err := p.file.Close(); err != nil {
-		_ = retained.Close()
-		return err
-	}
-
-	p.file = retained
-	p.info = retainedInfo
-	p.execPath = fmt.Sprintf("/proc/self/fd/%d", retained.Fd())
-	p.sealed = true
-
-	return nil
 }
 
 func (p *upgradeTargetPin) validate() error {
--- internal/daemon/upgrade_target_pin_linux.go
+++ internal/daemon/upgrade_target_pin_linux.go
@@ -2,6 +2,44 @@
 
 package daemon
 
+import (
+	"errors"
+	"fmt"
+
+	"github.com/d0ugal/graith/internal/executablepin"
+)
+
 func (p *upgradeTargetPin) retainPlatformCopy() error {
 	return p.retainLinuxCopy()
+}
+
+func (p *upgradeTargetPin) retainLinuxCopy() error {
+	retained, err := executablepin.SealedCopy(p.file, p.info.Size(), "graith-upgrade-target")
+	if err != nil {
+		return err
+	}
+
+	retainedInfo, err := retained.Stat()
+	if err != nil || !retainedInfo.Mode().IsRegular() || retainedInfo.Mode().Perm()&0o111 == 0 || retainedInfo.Size() != p.info.Size() {
+		_ = retained.Close()
+		return errors.New("retained upgrade target metadata is unsafe")
+	}
+
+	retainedDigest, err := digestUpgradeTargetFile(retained, retainedInfo.Size())
+	if err != nil || retainedDigest != p.digest {
+		_ = retained.Close()
+		return errors.New("retained upgrade target content differs")
+	}
+
+	if err := p.file.Close(); err != nil {
+		_ = retained.Close()
+		return err
+	}
+
+	p.file = retained
+	p.info = retainedInfo
+	p.execPath = fmt.Sprintf("/proc/self/fd/%d", retained.Fd())
+	p.sealed = true
+
+	return nil
 }
